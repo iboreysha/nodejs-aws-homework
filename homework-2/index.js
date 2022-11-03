4303bc09e7d1c50be5c18d61cb4248a272b1e412
MINSK\Igor_Boreysha <igor_boreysha@epam.com>
2022-11-03 16:07:10 +0300

    homework-2:  Added filter by isDeleted, in delete request updated "patch" to "delete"

--- homework-2/index.js
+++ homework-2/index.js
@@ -19,21 +19,22 @@ app.use(express.json());
 //
 const users = [
   {
-    "id": "718dcd31-9fa4-4517-8e78-2556f6b3df29", // it was "id":1,
+    "id": "718dcd31-9fa4-4517-8e78-2556f6b3df29",
     "login": "user1",
     "password": "pass1",
     "age": 10,
     "isDeleted": "false"
   },
   {
-    "id": "645b1056-d72c-4eda-bc48-fb4f8365136d", // it was "id":2,
+    "id": "645b1056-d72c-4eda-bc48-fb4f8365136d",
     "login": "user2",
     "password": "pass2",
     "age": 20,
     "isDeleted": "false"
   },
   {
-    "id": "cb8ed75b-05e9-4f1b-9b82-c4eebeb8620d", // it was "id":3,
+    "id": "cb8ed75b-05e9-4f1b-9b82-c4eebeb8620d",
+    "login": "user3",
     "password": "pass3",
     "age": 30,
     "isDeleted": "false"
@@ -56,17 +57,27 @@ app.get('/', (req, res) => {
 //
 app.get('/api/users', (req, res) => {
 
-  res.send(users);
+  // Filtered non deleted users
+  const usersList = users.filter((user) => user.isDeleted === "false");
+  res.send(usersList);
+
+  // For testing - See all users: active and deleted 
+  console.log("----------------------");
+  console.log(users);
+  console.log("----------------------");
 
 });
 
 
 //
-// Get one user by ID
+// Get user by ID
 //
 app.get('/api/users/:id', (req, res) => {
 
-  const user = users.find((user) => user.id === req.params.id);
+  // Filtered non deleted users
+  const usersList = users.filter((user) => user.isDeleted === "false");
+
+  const user = usersList.find((user) => user.id === req.params.id);
 
   if (!user) {
     res.status(404).send('The user with the given ID was not found');
@@ -94,7 +105,8 @@ app.post('/api/users', (req, res) => {
     login: req.body.login,
     password: req.body.password,
     age: req.body.age,
-    isDeleted: req.body.isDeleted
+    //isDeleted: req.body.isDeleted
+    isDeleted: "false"
   }
 
   users.push(user);
@@ -105,11 +117,14 @@ app.post('/api/users', (req, res) => {
 
 
 //
-// Update user
+// Update user by ID
 //
 app.put('/api/users/:id', (req, res) => {
 
-  const user = users.find((user) => user.id === req.params.id);
+  // Filtered non deleted users
+  const usersList = users.filter((user) => user.isDeleted === "false");
+
+  const user = usersList.find((user) => user.id === req.params.id);
 
   if (!user) {
     res.status(404).send('The user with the given ID was not found');
@@ -124,8 +139,7 @@ app.put('/api/users/:id', (req, res) => {
 
   user.login = req.body.login,
   user.password = req.body.password,
-  user.age = req.body.age,
-  user.isDeleted = req.body.isDeleted
+  user.age = req.body.age
 
   res.send(user);
 
@@ -135,9 +149,12 @@ app.put('/api/users/:id', (req, res) => {
 //
 // Mark user as deleted
 //
-app.patch('/api/users/:id', (req, res) => {
+app.delete('/api/users/:id', (req, res) => {
 
-  const user = users.find((user) => user.id === req.params.id);
+  // Filtered non deleted users
+  const usersList = users.filter((user) => user.isDeleted === "false");
+
+  const user = usersList.find((user) => user.id === req.params.id);
 
   if (!user) {
     res.status(404).send('The user with the given ID was not found');
@@ -145,12 +162,8 @@ app.patch('/api/users/:id', (req, res) => {
   }
 
   user.isDeleted = req.body.isDeleted;
-  // res.send(user);
 
-  const usersList = users.filter((user) => user.id !== req.params.id);
-  res.send(usersList);
-
-  console.log(`User with id ${user.id} has been deleted`);
+  res.send(`User is deleted ID: ${user.id}`);
 
 });
 
@@ -162,8 +175,8 @@ function validateUser(user) {
   const schema = Joi.object({
     login: Joi.string().min(3).max(20).required(),
     password: Joi.string().min(3).max(20).alphanum().required(),
-    age: Joi.number().integer().min(4).max(130).required(),
-    isDeleted: Joi.boolean().required()
+    age: Joi.number().integer().min(4).max(130).required()
+    //isDeleted: Joi.boolean().required()
   });
   return schema.validate(user);
 }
